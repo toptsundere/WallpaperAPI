c3e20747b9469f3ba0e49f5c1d59e74c2e9d3944
toptsundere <30511480+toptsundere@users.noreply.github.com>
2017-10-30 15:44:41 +0100

    Minor changes

--- app/src/main/java/com/bae/wallpapercolours/MainActivity.java
+++ app/src/main/java/com/bae/wallpapercolours/MainActivity.java
@@ -22,89 +22,80 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String hexColorPrimary;
+    String hexColorSecondary;
+    String hexColorTertiary;
+
+    ImageView wallpaperView;
+    ImageView pcCircleIcon;
+    TextView pcTextView;
+    TextView scTextView;
+    ImageView scCircleIcon;
+    TextView tcTextView;
+    ImageView tcCircleIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pcCircleIcon = findViewById(R.id.circlePrimary);
+        pcTextView = findViewById(R.id.primaryColourText);
+        scTextView = findViewById(R.id.secondaryColourText);
+        scCircleIcon = findViewById(R.id.circleSecondary);
+        tcTextView = findViewById(R.id.teritaryColourText);
+        tcCircleIcon = findViewById(R.id.circleTeritary);
 
+        //declare the IDs
+        wallpaperView = findViewById(R.id.wallpaperView);
 
         //get an instance of the wallpaper manager first
         final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
         final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
 
         //display the wallpaper
-        ImageView wallpaperView = findViewById(R.id.wallpaperView);
         wallpaperView.setImageDrawable(wallpaperDrawable);
 
         //create a wallpaper colors object
         WallpaperColors wallpaperColors = WallpaperColors.fromDrawable(wallpaperDrawable);
 
-
         /** PRIMARY COLOUR*/
         //retrieve the colors
         Color primaryColor = wallpaperColors.getPrimaryColor();
-        final String hexColorPrimary = String.format("#%06X", (0xFFFFFF & primaryColor.toArgb()));
-
+        hexColorPrimary = String.format("#%06X", (0xFFFFFF & primaryColor.toArgb()));
         //set the text with the primary colours
-        TextView pcTextView = findViewById(R.id.primaryColourText);
         pcTextView.setText(hexColorPrimary);
-
-        ImageView pcCircleIcon = findViewById(R.id.circlePrimary);
         pcCircleIcon.setColorFilter(Color.parseColor(hexColorPrimary));
 
         /** SECONDARY COLOUR */
         Color secondaryColor = wallpaperColors.getSecondaryColor();
-        final String hexColorSecondary = String.format("#%06X", (0xFFFFFF & secondaryColor.toArgb()));
-
-        TextView scTextView = findViewById(R.id.secondaryColourText);
+        hexColorSecondary = String.format("#%06X", (0xFFFFFF & secondaryColor.toArgb()));
         scTextView.setText(hexColorSecondary);
-
-        ImageView scCircleIcon = findViewById(R.id.circleSecondary);
         scCircleIcon.setColorFilter(Color.parseColor(hexColorSecondary));
 
         /** TERTIARY COLOUR*/
         Color tertiaryColor = wallpaperColors.getTertiaryColor();
-        final String hexColorTertiary = String.format("#%06X", (0xFFFFFF & tertiaryColor.toArgb()));
-
-        TextView tcTextView = findViewById(R.id.teritaryColourText);
+        hexColorTertiary = String.format("#%06X", (0xFFFFFF & tertiaryColor.toArgb()));
         tcTextView.setText(hexColorTertiary);
-
-        ImageView tcCircleIcon = findViewById(R.id.circleTeritary);
         tcCircleIcon.setColorFilter(Color.parseColor(hexColorTertiary));
 
+    }
 
-        /** COPY BUTTONS **/
-        ImageView copyPrimary = findViewById(R.id.copyPrimary);
-        ImageView copySecondary = findViewById(R.id.copySecondary);
-        ImageView copyTertiary = findViewById(R.id.copyTertiary);
+    public void copyPrimaryColor(View view){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(hexColorPrimary, hexColorPrimary);
+        clipboard.setPrimaryClip(clip);
+    }
 
-        copyPrimary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText(hexColorPrimary, hexColorPrimary);
-                clipboard.setPrimaryClip(clip);
-            }
-        });
+    public void copySecondaryColor(View view){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(hexColorSecondary, hexColorSecondary);
+        clipboard.setPrimaryClip(clip);
+    }
 
-        copySecondary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText(hexColorSecondary, hexColorSecondary);
-                clipboard.setPrimaryClip(clip);
-            }
-        });
-
-        copyTertiary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText(hexColorTertiary, hexColorTertiary);
-                clipboard.setPrimaryClip(clip);
-            }
-        });
+    public void copyTertiaryColor(View view){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(hexColorTertiary, hexColorTertiary);
+        clipboard.setPrimaryClip(clip);
     }
 
 }
